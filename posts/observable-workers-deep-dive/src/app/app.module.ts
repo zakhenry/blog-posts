3212042785625003430f3970a1c575de227e0714
Zak Henry <zak@cloudnc.com>
2019-08-18 16:59:54 +0100

    feat(Observable Worker Pool): Drop deprecated worker pool work

--- posts/observable-workers-deep-dive/src/app/app.module.ts
+++ posts/observable-workers-deep-dive/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BookSearchComponent } from './book-search/main-thread/book-search.component';
 import { BookSearchMultiWorkerComponent } from './book-search/worker-multi-thread/book-search-multi-worker.component';
-import { BookSearchWorkerPoolComponent } from './book-search/worker-pool-thread/book-search-worker-pool.component';
 import { BookSearchWorkerComponent } from './book-search/worker-thread/book-search-worker.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { BookSearchWorkerComponent } from './book-search/worker-thread/book-sear
     BookSearchComponent,
     BookSearchWorkerComponent,
     BookSearchMultiWorkerComponent,
-    BookSearchWorkerPoolComponent,
   ],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
